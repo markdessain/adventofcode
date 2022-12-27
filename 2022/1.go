22415514cb175a10106b33cce8bc3e7c9129bf0d
Mark Dessain <mark.dessain@gmail.com>
2022-12-27 09:06:41 +0000

    2022 day 1

--- 2022/1.go
+++ 2022/1.go
@@ -1,5 +1,47 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func main() {
-	
+	file, err := os.Open("./2022/data/1.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	index := 0
+	currentCount := 0
+	maxCount := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			if currentCount > maxCount {
+				maxCount = currentCount
+			}
+			currentCount = 0
+			index += 1
+		} else {
+			count, err := strconv.Atoi(line)
+
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				currentCount += count
+			}
+		}
+	}
+
+	fmt.Println(maxCount)
+	fmt.Println(index)
+
 }
