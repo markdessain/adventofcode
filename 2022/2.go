2a93e15c1e724cb5ac6a0e5ba60a68e4b767d743
Mark Dessain <mark.dessain@gmail.com>
2022-12-30 11:38:35 +0000

    part 2

--- 2022/2.go
+++ 2022/2.go
@@ -17,6 +17,7 @@ func main() {
 	scanner.Split(bufio.ScanLines)
 
 	score := 0
+	matchingscore := 0
 
 	winningMap := make(map[string]int)
 	winningMap["A X"] = 3 + 1
@@ -29,12 +30,25 @@ func main() {
 	winningMap["C Y"] = 0 + 2
 	winningMap["C Z"] = 3 + 3
 
+	matchingMap := make(map[string]int)
+	matchingMap["A X"] = 0 + 3
+	matchingMap["A Y"] = 3 + 1
+	matchingMap["A Z"] = 6 + 2
+	matchingMap["B X"] = 0 + 1
+	matchingMap["B Y"] = 3 + 2
+	matchingMap["B Z"] = 6 + 3
+	matchingMap["C X"] = 0 + 2
+	matchingMap["C Y"] = 3 + 3
+	matchingMap["C Z"] = 6 + 1
+
 	for scanner.Scan() {
 		line := scanner.Text()
 
 		score += winningMap[line]
+		matchingscore += matchingMap[line]
 	}
 
 	fmt.Println(score)
+	fmt.Println(matchingscore)
 
 }
